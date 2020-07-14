7104fbbed00b2446e71e43856a444c9b0d9f764c
xiaji <jwjwei@gmail.com>
2020-07-14 10:58:55 +0800

    divide and conquer

--- week2/SameTree.java
+++ week2/SameTree.java
@@ -1,7 +1,23 @@
 package week2;
 
 public class SameTree {
+  /**
+   * 二叉树的问题 左右子树 分治法
+   * @param p
+   * @param q
+   * @return
+   */
   public boolean isSameTree(TreeNode p, TreeNode q) {
-        
+    if (p == null && q == null) {
+      return true;
+    }
+    
+    if (p == null || q == null) {
+      return false;
+    }
+
+    boolean isLeftSameTree = isSameTree(p.left, q.left);
+    boolean isRightSameTree = isSameTree(p.right, q.right);
+    return isLeftSameTree & isRightSameTree & p.val == q.val;
   }
 }
