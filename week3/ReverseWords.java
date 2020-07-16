87cfad5a96964e1d8da3a42daa6d0d867ca18d97
xiaji <jwjwei@gmail.com>
2020-07-16 12:17:14 +0800

    add leetcode method with exist split method

--- week3/ReverseWords.java
+++ week3/ReverseWords.java
@@ -1,5 +1,9 @@
 package week3;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * 这个题目做过，但是没有review，第一个方法里有个知识点，就是空格split的结果是空串
  */
@@ -8,6 +12,7 @@ public class ReverseWords {
     if (s == null || s.isEmpty()) {
       return "";
     }
+    // 进化的split("\\s + ") 但是仍然是留下开头全是空的情况;
     String[] ss = s.split(" ");
     int index = ss.length - 1;
     //StringBuilder 线程不安全
@@ -24,5 +29,39 @@ public class ReverseWords {
       sb.append(ss[index--]);
     }
     return sb.toString();
+  }
+  // leetcode split version ans
+  public String reverseWordsLeetCode(String s) {
+    // remove leading spaces
+    s = s.trim();
+    // split by multiple spaces
+    List<String> wordList = Arrays.asList(s.split("\\s+"));
+    Collections.reverse(wordList);
+    return String.join(" ", wordList);
+  }
+
+  // 如果不让用split函数 或者说trim函数，trim:remove leading space
+  // 相向双指针
+  public String reverseWords2(String s) {
+    if (s == null || s.isEmpty()) {
+      return "";
+    }
+    char[] chs = reverseString(s);
+    int left = 0, right = chs.length - 1;
+    while (left < right) {
+      
+    }
+  }
+
+  private char[] reverseString(String s) {
+    int left = 0, right = s.length() - 1;
+    char[] chs = s.toCharArray();
+    char temp = '0';
+    while (left < right) {
+      temp = chs[right];
+      chs[right--] = chs[left];
+      chs[left++] = temp;
+    }
+    return chs;
   }
 }
