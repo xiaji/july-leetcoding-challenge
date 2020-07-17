80a76e7f8fb7b737a7b60e0dff2cff8f9f810ce4
xiaji <jwjwei@gmail.com>
2020-07-17 12:49:11 +0800

    update my method

--- week3/Power.java
+++ week3/Power.java
@@ -21,9 +21,24 @@ public class Power {
     return n == Integer.MIN_VALUE ? helper(x, newN) * x : helper(x, newN);    
   }
 
+  // update above method
+  public double myPow1(double x, int n) {
+    boolean isNegative = false;
+    if (n < 0) {
+      isNegative = true;
+      x = 1.0 / x;
+      // avoid over boundary
+      n = - (n + 1);
+    }
+    return isNegative ? helper(x, n) * x : helper(x, n);
+  }
+
   private double helper(double x, int n) {
     // mistake: one condition n == 1
     // or result would be 1.0 always
+    if (n == 1) {
+      return x;
+    }
     if (n == 0) {
       return 1.0;
     }
@@ -50,5 +65,29 @@ public class Power {
       return temp * temp * x;
     }
     return temp * temp / x;
+  }
+
+  // for loop method
+  public double myPow3(double x, int n) {
+    if (n == 0) {
+      return 1.0;
+    }
+    boolean isNegative = false;
+    if (n < 0) {
+      x = 1 / x;
+      n = - (n + 1);
+      isNegative = true;
+    }
+
+    double ans = 1.0, base = x;
+    while (n != 0) {
+      if (n % 2 == 1) {
+        ans *= base;
+      }
+      base *= base;
+      n /= 2;
+    }
+
+    return isNegative ? ans * x : ans;
   }
 }
