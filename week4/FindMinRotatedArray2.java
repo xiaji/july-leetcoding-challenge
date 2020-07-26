030907019ef2d025023734215cfd6b00eab1ac43
xiaji <jwjwei@gmail.com>
2020-07-26 15:43:00 +0800

    o(N)

--- week4/FindMinRotatedArray2.java
+++ week4/FindMinRotatedArray2.java
@@ -1,5 +1,20 @@
 package week4;
 
+/**
+ * 有重复元素存在就直接搜
+ * 为什么？
+ */
 public class FindMinRotatedArray2 {
-  
+  public int findMin(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return -1;
+    }    
+    int min = Integer.MAX_VALUE;
+    for (int num : nums) {
+      if (num < min) {
+        min = num;
+      }  
+    }
+    return min;
+  }
 }
