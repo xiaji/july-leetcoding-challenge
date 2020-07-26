406dc415d0ef7c0e53093bf043b056d4cfd19887
xiaji <jwjwei@gmail.com>
2020-07-26 15:55:38 +0800

    o(N) works

--- week4/AddDigits.java
+++ week4/AddDigits.java
@@ -1,5 +1,18 @@
 package week4;
 
 public class AddDigits {
-  
+  public int addDigits(int num) {
+    if (num <= 9) {
+      return num;
+    }
+    int carry = 0;
+    while (num != 0) {
+      carry += num % 10;
+      if (carry > 9) {
+        carry = carry % 10 + carry / 10;
+      }
+      num /= 10;
+    }
+    return carry;   
+  }
 }
