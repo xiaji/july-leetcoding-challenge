75082874157c05406ec8dd9fe0703df94118c507
xiaji <jwjwei@gmail.com>
2020-07-28 13:29:07 +0800

    the first version does not work

--- week4/ConstructBinaryTreeInAndPostOrder.java
+++ week4/ConstructBinaryTreeInAndPostOrder.java
@@ -3,10 +3,13 @@ package week4;
 /**
  * binray tree traversal 基础题目
  * 用inorder来构建 postorder最后一个就是根
+ * mistake：不要去找两边各自的分界点，找长度，这样只需要扫描数组一遍
  */
 public class ConstructBinaryTreeInAndPostOrder {
   public TreeNode buildTree(int[] inorder, int[] postorder) {
-    if (inorder == null || postorder == null || inorder.length == 0 || postorder.length == 0) {
+    // miss one condition
+    if (inorder == null || postorder == null || inorder.length == 0 || postorder.length == 0
+      || inorder.length != postorder.length) {
       return new TreeNode();
     }
     return helper(0, inorder.length - 1, inorder, 0, postorder.length - 1, postorder);    
@@ -14,14 +17,22 @@ public class ConstructBinaryTreeInAndPostOrder {
 
   private TreeNode helper(int inStartIdx, int inEndIdx, int[] inorder, int postStartIdx, 
     int postEndIdx, int[] postorder) {
-      if (inStartIdx > inEndIdx) {
+      // miss one condition
+      if (inStartIdx > inEndIdx || postStartIdx > postEndIdx) {
         return null;
       }
       TreeNode root = new TreeNode(postorder[postEndIdx]);
-      int rootIdx = findIndex(postorder[postEndIdx], inorder);
-      root.left = helper(inStartIdx, rootIdx - 1, inorder, postStartIdx, postEndIdx, postorder)
+      int idx = inStartIdx;
+      while (idx <= inEndIdx && inorder[idx] != postorder[postEndIdx]) {
+        idx++;
+      }
+      int leftLength = idx - inStartIdx;
+      root.left = helper(inStartIdx, idx - 1, inorder, postStartIdx, postStartIdx + leftLength - 1, postorder);
+      root.right = helper(idx + 1, inEndIdx, inorder, postStartIdx + leftLength, postEndIdx - 1, postorder);
+      return root;
   }
 
+  // useless method
   private int findIndex(int val, int[] nums) {
     int idx = 0;
     for (int num : nums) {
