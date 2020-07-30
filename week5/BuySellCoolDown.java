1975ae24e3f275417a68a7f0473915ffd40c7b6f
xiaji <jwjwei@gmail.com>
2020-07-30 13:05:36 +0800

    my method can not get the right ans

--- week5/BuySellCoolDown.java
+++ week5/BuySellCoolDown.java
@@ -2,15 +2,16 @@ package week5;
 
 /**
  * 无限次的买卖，但是在卖之后必须休息一天
+ * while循环尤其是跟数组index相关时 一定要把边界大小放在最前面
  */
 public class BuySellCoolDown {
   public int maxProfit(int[] prices) {
     if (prices == null || prices.length == 0) {
       return 0;
     }    
-    int res = 0, i = 1;
-    while (i != prices.length) {
-      while (prices[i] - prices[i - 1] > 0) {
+    int res = 0, i = 1, sz = prices.length;
+    while (i < sz) {
+      while (i < sz && prices[i] - prices[i - 1] > 0) {
         res += prices[i] - prices[i - 1];
         i++;
       }
