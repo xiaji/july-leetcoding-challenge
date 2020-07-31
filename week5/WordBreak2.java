fde9f4d14e018b6e8f84b58895c65017cba555be
xiaji <jwjwei@gmail.com>
2020-07-31 12:59:54 +0800

    word break 2

---
+++ week5/WordBreak2.java
@@ -0,0 +1,12 @@
+package week5;
+
+import java.util.List;
+
+public class WordBreak2 {
+  public List<String> wordBreak(String s, List<String> wordDict) {
+    List<String> res = new ArrayList<>();
+    if (s == null || s.isEmpty() || wordDict == null || wordDict.isEmpty()) {
+      return res;
+    }  
+  }
+}
