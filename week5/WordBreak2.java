f4fe0ff15cd0ad9d6127f6fdc116fbc0e8763c28
xiaji <jwjwei@gmail.com>
2020-07-31 13:29:49 +0800

    my method

--- week5/WordBreak2.java
+++ week5/WordBreak2.java
@@ -1,5 +1,6 @@
 package week5;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WordBreak2 {
@@ -7,6 +8,27 @@ public class WordBreak2 {
     List<String> res = new ArrayList<>();
     if (s == null || s.isEmpty() || wordDict == null || wordDict.isEmpty()) {
       return res;
-    }  
+    }
+    // StringBuffer sb = new StringBuffer();
+    char[] chs = s.toCharArray();
+    dfs(chs, new ArrayList<>(), 0, wordDict, res);
+    return res;
+  }
+
+  private void dfs(char[] chs, List<String> subList, int start, List<String> dict, List<String> res) {
+    if (start == chs.length) {
+      res.add(String.join(" ", new ArrayList<>(subList)));
+      return;
+    }
+    StringBuffer temp = new StringBuffer();
+    for (int i = start; i < chs.length; ++i) {
+      temp.append(chs[i]);
+      if (!dict.contains(temp.toString())) {
+        continue;
+      }
+      subList.add(temp.toString());
+      dfs(chs, subList, i + 1, dict, res);
+      subList.remove(subList.size() - 1);
+    }
   }
 }
