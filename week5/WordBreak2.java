bfeebc95528d8c905499440b0e9ab340a3954912
xiaji <jwjwei@gmail.com>
2020-08-01 15:27:38 +0800

    my method run overtime

--- week5/WordBreak2.java
+++ week5/WordBreak2.java
@@ -3,6 +3,13 @@ package week5;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 找所有的可能性，就是用DFS
+ * 递归的定义 递归的拆解 递归的出口
+ * subList里存入的是分割出来的单词 如果start == s的长度 那说明分的刚好 可以存入
+ * 之后trie，或者说按照int[26]那个方法 可以根据前缀 加速for循环的结束
+ * String.join()方法
+ */
 public class WordBreak2 {
   public List<String> wordBreak(String s, List<String> wordDict) {
     List<String> res = new ArrayList<>();
